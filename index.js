d289bab55e73e61bfeb023267faf488fe850c9ff
ethan5432 <ethan.nirenberg@gmail.com>
2023-09-18 13:50:20 -0400

    movie cards

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
-
-
-const apiKey = '2f7d241'
-const movieTitle = 'Jaws'
-const apiUrl = `http://www.omdbapi.com/?apikey=${apiKey}&t=${movieTitle}`
+const apiKey = "2f7d241";
+const movieTitle = "Jaws";
+const apiUrl = `http://www.omdbapi.com/?apikey=${apiKey}&t=${movieTitle}`;
 
 async function getMovie() {
-    const movies = await fetch(apiUrl);
-   const movieData = await movies.json();
-    console.log(
-        movieData.map((movie) => ` <div class="user-card">
-    <div class="user-card__container">
-        <h3>Movies Name</h4>
-        <p><b>Email:</b> email@email.com</p>
-        <p><b>Phone:</b> 0000000000</p>
-        <p><b>Website:</b> <a href="https://website.website" target="_blank">website.website</a></p>
-    </div>
-    </div>`)
-        .join("")
-    );
+  const movies = await fetch(apiUrl);
+  const movieData = await movies.json();
+  console.log(
+    movieData
+      .map(
+        (movie) => ` <div class="user-card">
+            <div class="user-card__container">
+              <h3>Movies Title</h3>
+                <p><b>Release:</b> email@email.com</p>
+                <p><b>Rating:</b> 0000000000</p>
+                <p><b>RunTime:</b> <a href="https://website.website" target="_blank">website.website</a></p>
+                <p><b>Genre:</b> <a href="https://website.website" target="_blank">website.website</a></p>
+                <p><b>Actors:</b> <a href="https://website.website" target="_blank">website.website</a></p>
+                <p><b>Description:</b> <a href="https://website.website" target="_blank">website.website</a></p>
+                <p><b>Awards:</b> <a href="https://website.website" target="_blank">website.website</a></p>
+            </div>
+          </div>`
+      )
+      .join("")
+  );
 }
 
 getMovie();
