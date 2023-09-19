0902e2e4b872e48416f9b062eff2af3888fd714e
ethan5432 <ethan.nirenberg@gmail.com>
2023-09-19 14:10:41 -0400

    figuring out the search method

--- index.js
+++ index.js
@@ -4,30 +4,33 @@ const apiUrl = `http://www.omdbapi.com/?apikey=${apiKey}&s=${movieTitle}`;
 
 async function getMovie() {
   const movies = await fetch(apiUrl);
-    const movieData = await movies.json();
-    const movieListEl = document.querySelector(".movie-list")
-  console.log(movieData)
-    movieListEl.innerHTML = movieData.Search
-      .map(
-        (movie) => movieHTML(movie)).join("");
-  }
-    
+  const movieData = await movies.json();
+  const movieListEl = document.querySelector(".movie-list");
+  movieListEl.innerHTML = movieData.Search.map((movie) =>
+    movieHTML(movie)
+  ).join("");
+}
 
 getMovie();
 
-
 function movieHTML(movie) {
-    return ` <div class="movie-card">
+  return `
+  <div class="movie-card">
+            <figure>
+                <img class="posterImage" src="${movie.Poster}">
+            </figure>
             <div class="movie-card__container">
             
               <h3>${movie.Title}</h3>
-                <p><b>Release:</b> ${movie.Year}</p>
-                <p><b>Rating:</b> ${movie.Title}</p>
-                <p><b>RunTime:</b>${movie.imdbID}</p>
-                <p><b>Genre:</b>${movie.Title}</p>
-                <p><b>Actors:</b>${movie.Title}</p>
-                <p><b>Description:</b>${movie.Title}</p>
-                <p><b>IMDB ID:</b>${movie.imdbID}</p>
+                <p><b>Release: </b> ${movie.Year}</p>
+                <p><b>Type: </b>${movie.Type}</p>
+                <p><b>IMDB ID: </b>${movie.imdbID}</p>
             </div>
           </div>`;
 }
+
+function onSearchChange(movie) {
+  const movieQuery = movie.target.value;
+}
+
+onSearchChange();
